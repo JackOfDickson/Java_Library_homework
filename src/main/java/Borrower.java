20be1ea53d1c1b4c38af4790acdac9bd36b73248
Jack Dickson <jackowendickson@gmail.com>
2022-05-10 15:50:53 +0100

    Added Borrower and tested collection

---
+++ src/main/java/Borrower.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Borrower {
+
+    private ArrayList<Book> collection;
+
+    public Borrower(){
+        this.collection = new ArrayList<>();
+    }
+
+    public int collectionCount(){
+        return collection.size();
+    }
+}
